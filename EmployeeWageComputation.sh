92424365e0bd37c40fbff54a9982a5c7184955b0
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-06 19:19:27 +0530

    calculating salary using switch case

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -8,11 +8,21 @@ FULL_DAY_WORKING_HOUR=8
 HALF_DAY_WORKING_HOUR=4
 #variable
 randomPresent=$((RANDOM%2))
+randomTime=$((RANDOM%2+1))
+workingHour=0
+function getWorkingSalary()
+{
+	case $randomTime in
+	1) workingHour=FULL_DAY_WORKING_HOUR;;
+	2) workingHour=HALF_DAY_WORKING_HOUR;;
+	esac
+	echo $workingHour
+}
 
 if [ $randomPresent -eq 1 ]
 then
-	dailyWage=$(( $WORKING_HOUR * $WAGE_PER_HOUR ))
-	echo "Present"
+	workingHour=$(getWorkingSalary)
+	dailyWage=$(($workingHour * $WAGE_PER_HOUR))
 	echo "Daily Wage :"$dailyWage
 else
 	echo "Absent"
