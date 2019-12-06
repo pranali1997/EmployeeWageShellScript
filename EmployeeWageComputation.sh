b45e283e834e271c63c4bf6dfbc4d71a96aae256
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-06 18:37:21 +0530

    Calculating Daily Wage of Employee

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -2,11 +2,17 @@
 
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
 
+#constant
+WAGE_PER_HOUR=20
+WORKING_HOUR=8
+#variable
 randomPresent=$((RANDOM%2))
 
 if [ $randomPresent -eq 1 ]
 then
+	dailyWage=$(( $WORKING_HOUR * $WAGE_PER_HOUR ))
 	echo "Present"
+	echo "Daily Wage :"$dailyWage
 else
 	echo "Absent"
 fi
