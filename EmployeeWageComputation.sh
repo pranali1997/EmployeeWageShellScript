46475f2cfc2c3f2a8023cc6ba0b40b291493e30d
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-07 12:48:18 +0530

    get wages for a month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -6,24 +6,31 @@ echo "WELCOME TO EMPLOYEE WAGE COMPUTATION"
 WAGE_PER_HOUR=20
 FULL_DAY_WORKING_HOUR=8
 HALF_DAY_WORKING_HOUR=4
+WORKING_DAYS=20
 #variable
-randomPresent=$((RANDOM%2))
 randomTime=$((RANDOM%2+1))
 workingHour=0
+totalWage=0
+dailyWage=0
 function getWorkingSalary()
 {
 	case $randomTime in
-	1) workingHour=FULL_DAY_WORKING_HOUR;;
-	2) workingHour=HALF_DAY_WORKING_HOUR;;
+	1) workingHour=$FULL_DAY_WORKING_HOUR;;
+	2) workingHour=$HALF_DAY_WORKING_HOUR;;
 	esac
 	echo $workingHour
 }
 
-if [ $randomPresent -eq 1 ]
-then
-	workingHour=$(getWorkingSalary)
-	dailyWage=$(($workingHour * $WAGE_PER_HOUR))
-	echo "Daily Wage :"$dailyWage
-else
-	echo "Absent"
-fi
+for((day=1;day<=20;day++))
+do
+	randomPresent=$((RANDOM%2))
+	if [ $randomPresent -eq 1 ]
+	then
+		workingHour=$(getWorkingSalary)
+		dailyWage=$(($workingHour * $WAGE_PER_HOUR))
+		echo "present wage: "$dailyWage
+	else
+		echo "Absent"
+	fi
+done
+
